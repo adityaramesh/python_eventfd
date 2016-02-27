adf868f61d50ed21b03adfeba8e15c0c75667ee2
Aditya Ramesh <_@adityaramesh.com>
2016-02-27 04:56:34 -0500

    Fixed issue with shared library not being copied.

--- eventfd/__init__.py
+++ eventfd/__init__.py
@@ -1,1 +1,4 @@
-from eventfd._eventfd import __doc__, Event, EFD_CLOEXEC, EFD_NONBLOCK, EFD_SEMAPHORE
+try:
+	from _eventfd import __doc__, Event, EFD_CLOEXEC, EFD_NONBLOCK, EFD_SEMAPHORE
+except ImportError:
+	from eventfd._eventfd import __doc__, Event, EFD_CLOEXEC, EFD_NONBLOCK, EFD_SEMAPHORE
--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
 	name         = 'eventfd',
 	version      = '0.1',
 	description  = 'Adds support for eventfd to Python 3.',
-	url          = '',
+	url          = 'https://github.com/adityaramesh/python_eventfd',
 	author       = 'Aditya Ramesh',
 	author_email = '_@adityaramesh.com',
 	license      = 'BSD',
-	packages     = ['eventfd']
+	packages     = ['eventfd'],
+	package_data = {'eventfd': ['_eventfd.so']}
 )
